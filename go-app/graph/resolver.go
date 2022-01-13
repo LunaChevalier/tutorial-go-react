c3407c183ac2722a24d1233ae715a73ac1496ce9
bravo.miyama <bravo.miyama@karabiner.tech>
2022-01-13 18:48:51 +0900

    [update] DBからデータを取得する
    - DBに登録してある、Todoというデータを取得するように修正した

--- go-app/graph/resolver.go
+++ go-app/graph/resolver.go
@@ -1,6 +1,7 @@
 package graph
 import (
 	"context"
+	"github.com/jinzhu/gorm"
 	"github.com/LunaChevalier/tutorial-go-react/graph/generated"
 	"github.com/LunaChevalier/tutorial-go-react/graph/model"
 )
@@ -9,7 +10,9 @@ import (
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	DB *gorm.DB
+}
 
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
@@ -35,24 +38,29 @@ func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) 
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	return []*model.Todo{
-		&model.Todo{
-				ID:   "todo001",
-				Text: "部屋の掃除",
-				Done: false,
-				User: &model.User{
-						ID:   "user001",
-						Name: "たろ1",
-				},
-		},
-		&model.Todo{
-				ID:   "todo002",
-				Text: "買い物",
-				Done: true,
-				User: &model.User{
-						ID:   "user001",
-						Name: "たろ1",
-				},
-		},
-	},nil
+	todos := []*model.Todo{}
+	r.DB.Find(&todos)
+	return todos, nil
+	
+	// こんな形のmodelがレスポンスされる
+	// return []*model.Todo{
+	// 	&model.Todo{
+	// 			ID:   "todo001",
+	// 			Text: "部屋の掃除",
+	// 			Done: false,
+	// 			User: &model.User{
+	// 					ID:   "user001",
+	// 					Name: "たろ1",
+	// 			},
+	// 	},
+	// 	&model.Todo{
+	// 			ID:   "todo002",
+	// 			Text: "買い物",
+	// 			Done: true,
+	// 			User: &model.User{
+	// 					ID:   "user001",
+	// 					Name: "たろ1",
+	// 			},
+	// 	},
+	// },nil
 }
